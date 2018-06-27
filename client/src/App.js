0445ab4196da0cfd12e978ce74e50d32e7be41cc
Hunter Wiley <hunterwiley88@gmail.com>
2018-06-27 13:35:37 -0700

    signUp form and signUp.js routes in api.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,19 @@ import NavBar from './Components/NavBar';
 import Players from "./pages/Players";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import API from "./utils/API";
+import Signup from "./pages/SignUp"
 
 class App extends Component {
+
+
+  handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
+    event.preventDefault();
+    API.getPlayers(this.state.players)
+      .then(res => this.setState({ players: res.data }))
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
@@ -17,6 +28,7 @@ class App extends Component {
               <Route exact path="/" component={Home} />
               <Route exact path="/players" component={Players} />
               <Route exact path="/players/:id" component={Profile} />
+              <Route exact path="/signup" component={Signup} />
             </Switch>
           </div>
         </Router>
---
+++ client/src/pages/SignUp/SignUp.js
@@ -0,0 +1,111 @@
+
+import React, { Component } from "react"
+import API from "../../utils/API";
+
+class SignUp extends React.Component {
+
+     // Setting the component's initial state
+  state = {
+    username: "",
+    password: "",
+    gameName: "",
+    rank: "",
+    role: "",
+    captain: false,
+
+} 
+
+handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = event.target;
+
+    //Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    API.signUp({
+        username: this.state.username,
+        password: this.state.password,
+        gameName: this.state.gameName,
+        rank: this.state.rank,
+        role: this.state.role,
+        captain: this.state.captain,
+    })
+
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+
+  };
+
+  render() {
+    // 
+    return (
+        <div class="container">
+        
+        <form className="form">
+          <input
+            value={this.state.username}
+            name="username"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Username"
+          />
+          <input
+            value={this.state.password}
+            name="password"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Password"
+          />
+          <input
+            value={this.state.gameName}
+            name="gameName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="In-Game-Name"
+          />
+          <input
+            value={this.state.rank}
+            name="rank"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="rank"
+          />
+          <input
+            value={this.state.role}
+            name="role"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Role"
+          />
+          <input
+            value={this.state.captain}
+            name="role"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Captain"
+          />
+          <button onClick={this.handleFormSubmit}>Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+
+
+// add put request w/axios
+// add componentwillmount()
+
+export default SignUp;
+
+
