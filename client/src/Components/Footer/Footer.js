149b4e5e4a88758e83457fd2579db27463115c69
jodecile84 <jodecile84@gmail.com>
2018-06-27 20:43:32 -0700

    changed video,

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -8,10 +8,8 @@ class Footer extends Component {
 
       <footer>
         <div class="container">
-        <a href="#!" className="lol-logo"><img src="https://cdn.shopify.com/s/files/1/3101/3620/products/1_LoL_Logo_editado_300x300.png?v=1519654839" alt="logo" />
-          <b>CLASH</b>COMP</a>
-          © 2014 Copyright Text
-          <a href="#!">More Links</a>
+          <span id="copyright"> <b>CLASH</b>COMP © 2018 Copyright</span>
+          
         </div>
       </footer>
       );
